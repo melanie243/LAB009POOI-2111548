4020775413fec5553e2f0b70ea97c7f1a316f3d8
CarlosSomoza <142445282+CarlosSomoza@users.noreply.github.com>
2023-10-29 18:41:06 -0500

    Update Ejecicio4POO1.java

--- Ejecicio4POO1.java
+++ Ejecicio4POO1.java
@@ -2,6 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+
+//en los "//" sabran donde va que para que se guien
+
 package ejecicio4poo1;
 import java.util.List;
 import java.util.Scanner;
@@ -36,141 +39,147 @@ public class Ejecicio4POO1 {
 
             switch (opcion) {
                 case 1:
-                    // Agregar un nuevo estudiante
-                    System.out.println("INGRESE NOMBRES Y APELLIDOS:");
+                    // REGISTRAR A UN NUEVO ESTUDIANTE
+                    System.out.println("****** REGISTRAR A UN NUEVO ESTUDIANTE ******** ");
+                    System.out.print("Ingrese NOMBRES: ");
                     String nombres = scanner.nextLine();
-                    System.out.println("INGRESE CICLO:");
+                    System.out.print("Ingrese APELLIDOS: ");
+                    String apellidos = scanner.nextLine();
+                    System.out.print("Ingrese CICLO: ");
                     int ciclo = scanner.nextInt();
-                    scanner.nextLine(); // Para consumir el salto de línea después de la entrada numérica.
-                    System.out.println("INGRESE PENSION:");
+                    System.out.print("Ingrese PENSION: ");
                     double pension = scanner.nextDouble();
-
-                    Estudiante estudiante = new Estudiante( ciclo, nombres, ciclo, pension);
-                    estudiantes.adicionar(estudiante);
+                    Estudiante nuevoEstudiante = new Estudiante(estudiantes2023.tamano() + 1, nombres + " " + apellidos, ciclo, pension);
+                    estudiantes2023.adicionar(nuevoEstudiante);
+                    System.out.println("¡Se añadió con éxito!");
                     break;
-                    
-               case 2:
-                    System.out.println("****** BUSCAR A UN ESTUDIANTE ********");
-                    System.out.println("PUEDE BUSCAR POR NOMBRE O CODIGO. ELEGIR UNA OPCIÓN:");
-                    System.out.println("A – APELLIDO");
-                    System.out.println("B – CODIGO");
-                    String buscarPor = scanner.nextLine();
 
-                    if (buscarPor.equalsIgnoreCase("A")) {
-                        // Aquí puedes implementar la búsqueda por apellido
-                    } else if (buscarPor.equalsIgnoreCase("B")) {
-                        System.out.print("INGRESE EL CÓDIGO DEL ALUMNO: ");
-                        int codigoBuscado = scanner.nextInt();
-                        boolean encontrado = false;
-                        for (Estudiante estudiante : Estudiantes2023.getEstudiante()) {
-                            if (estudiante.getCodigo() == codigoBuscado) {
-                                System.out.println("El alumno es: " + estudiante);
-                                encontrado = true;
-                                break;
-                            }
+                case 2:
+                    // BUSCAR A UN ESTUDIANTE
+                    System.out.println("****** BUSCAR A UN ESTUDIANTE ******");
+                    System.out.println("Seleccione una opción de búsqueda:");
+                    System.out.println("A - Búsqueda por Código");
+                    System.out.println("B - Búsqueda por Apellido");
+                    String opcionBusqueda = scanner.nextLine();
+                    
+                    if (opcionBusqueda.equalsIgnoreCase("A")) {
+                        //Codigo
+                        System.out.print("Ingrese el código del estudiante a buscar: ");
+                        int codigo = scanner.nextInt();
+                        Estudiante estudianteEncontrado = estudiantes2023.buscar(codigo);
+
+                        if (estudianteEncontrado != null) {
+                            System.out.println("Datos del estudiante:\n" + estudianteEncontrado.toString());
+                        } else {
+                            System.out.println("El estudiante no está registrado.");
                         }
-                        
-                        if (!encontrado) {
-                            System.out.println("El alumno no está registrado.");
+                    } else if (opcionBusqueda.equalsIgnoreCase("B")) {
+                        //Apellido
+                        System.out.print("Ingrese el apellido del estudiante a buscar: ");
+                        String apellidoBuscar = scanner.nextLine();
+                        Estudiante estudiantePorApellido = estudiantes2023.buscarPorApellido(apellidoBuscar);
+
+                        if (estudiantePorApellido != null) {
+                            System.out.println("Datos del estudiante:\n" + estudiantePorApellido.toString());
+                        } else {
+                            System.out.println("No hay estudiantes registrados con el apellido " + apellidoBuscar);
                         }
+                    } else {
+                        System.out.println("Opción de búsqueda no válida.");
                     }
                     break;
-                    
+
                 case 3:
-                      System.out.print("Ingrese el código del estudiante a eliminar: ");
-                    int codigoEliminar = scanner.nextInt();
-                    estudiante estudianteAEliminar = estudiantes.buscar(codigoEliminar);
+                    // ELIMINACIÓN DEL ESTUDIANTE
+                    System.out.println("****** ELIMINACIÓN DEL ESTUDIANTE ********");
+                    System.out.print("Ingrese ID del estudiante a eliminar: ");
+                    int idEstudianteEliminar = scanner.nextInt();
+                    Estudiante estudianteAEliminar = estudiantes2023.buscar(idEstudianteEliminar);
 
                     if (estudianteAEliminar != null) {
-                        estudiantes.eliminarEstudiante(estudianteAEliminar);
-                        System.out.println("Estudiante eliminado correctamente.");
+                        estudiantes2023.eliminar(estudianteAEliminar);
+                        System.out.println("¡Se eliminó con éxito!");
                     } else {
-                        System.out.println("Estudiante no encontrado.");
-                    }
-                    break;  
-    
-                 case 4:
-                      System.out.println("****** MODIFICACIÓN ESTUDIANTE ********");
-                      System.out.println("INDIQUE EL CÓDIGO DEL ESTUDIANTE A MODIFICAR:");
-    
-                     int codigoModificar = scanner.nextInt();
-                     scanner.nextLine(); // Consumir la línea en blanco
-
-                    System.out.print("INGRESE NOMBRES: ");
-                    String nombres = scanner.nextLine();
-
-                    System.out.print("INGRESE APELLIDOS: ");
-                    String apellidos = scanner.nextLine();
-
-                    System.out.print("INGRESE CICLO: ");
-                    String ciclo = scanner.nextLine();
-
-                    System.out.print("INGRESE PENSION: ");
-                    double pension = scanner.nextDouble();
-
-                   if (estudiantes2023.modificarEstudiante(codigoModificar, nombres, apellidos, ciclo, pension)) {
-                    System.out.println("¡Se modificó con éxito!");
-                   } else {
-                   System.out.println("No se encontró al estudiante a modificar.");
-                   }
-                   break;
-                   
-                   case 5:
-    // Modificar apellidos en minúsculas
-                    System.out.print("Ingrese el código del estudiante cuyos apellidos desea modificar: ");
-                    int codigoBusqueda = scanner.nextInt();
-                    Estudiante estudianteAModificar = estudiantes.buscar(codigoBusqueda);
-
-                    if (estudianteAModificar != null) {
-                        System.out.print("Ingrese los nuevos apellidos en minúsculas: ");
-                        String nuevosApellidos = scanner.nextLine().toLowerCase();
-                        estudianteAModificar.setApellidos(nuevosApellidos);
-                        System.out.println("Apellidos modificados correctamente.");
-                    } else {
-                        System.out.println("Estudiante no encontrado.");
+                        System.out.println("No se encontró un estudiante con el ID ingresado.");
                     }
                     break;
-                    
-                    case 6:
-                     estudiantes.listarEstudiantes();
-                     break;
-                    
-                   case 7:
-                   List<Estudiante> estudiantesPorApellidos = Estudiantes.getEstudiantesPorApellidos();
-    
-                    if (estudiantesPorApellidos.isEmpty()) {
-                   System.out.println("No hay estudiantes registrados por apellidos.");
-                    } else {
-                   System.out.println("Estudiantes por apellidos:");
-                    for (estudiante estudiante : estudiantesPorApellidos) {
-                    System.out.println(estudiante);
-        }
-    }
-    break;
-    
-                case 8:
-                System.out.print("¿Ordenar en orden ascendente (Sí/No)? ");
-                String ordenAscendenteStr = scanner.nextLine().toLowerCase();
-                boolean ordenAscendente = ordenAscendenteStr.equals("si") || ordenAscendenteStr.equals("sí");
-                Estudiantes.verEstudiantesPorPension(ordenAscendente);
-                break;
-    
-                case 9:
-                  double totalPensiones = estudiantes.calcularTotalPensiones();
-                  System.out.println("****** SUMATORIA DE PENSIONES ********");
-                  System.out.println("Lista de estudiantes:");
-                  estudiantes.verTodosLosEstudiantes();
-                  System.out.println("LA SUMA TOTAL ES: S/ " + totalPensiones);
-                 break;
 
-                // Implementa otras opciones del menú aquí.
+                case 4:
+                    // MODIFICACION DEL ESTUDIANTE
+                    System.out.println("****** MODIFICACION DEL ESTUDIANTE ********");
+                    System.out.print("Ingrese el código del estudiante a modificar: ");
+                    int codigoModificacion = scanner.nextInt();
+                    Estudiante estudianteModificar = estudiantes2023.buscar(codigoModificacion);
+
+                    if (estudianteModificar != null) {
+                        System.out.print("Ingrese el nuevo nombre: ");
+                        String nuevoNombre = scanner.nextLine();
+                        System.out.print("Ingrese el nuevo ciclo: ");
+                        int nuevoCiclo = scanner.nextInt();
+                        System.out.print("Ingrese la nueva pensión: ");
+                        double nuevaPension = scanner.nextDouble();
+
+                        Estudiante nuevoEstudianteModificado = new Estudiante(estudianteModificar.getCodigo(), nuevoNombre, nuevoCiclo, nuevaPension);
+                        estudiantes2023.modificarEstudiante(estudianteModificar, nuevoEstudianteModificado);
+                        System.out.println("Estudiante modificado con éxito");
+                    } else {
+                        System.out.println("Estudiante no encontrado");
+                    }
+                    break;
+
+                case 5:
+                    // MODIFICACION ESTUDIANTE MINUSCULA
+                    estudiantes2023.modificarApellidosMayuscula();
+                    System.out.println("****** MODIFICACION ESTUDIANTE MINUSCULA ********");
+                    System.out.println("Apellidos en Minúsculas:");
+                    for (Estudiante estudiante : estudiantes2023.getEstudiantes()) {
+                        System.out.println(estudiante.getCodigo() + " " + estudiante.getNombre() + " " + estudiante.getCiclo() + " " + estudiante.getPension());
+                    }
+                    break;
+
+                case 6:
+                    // LISTA DE ESTUDIANTES
+                    System.out.println("****** LISTA DE ESTUDIANTES ********");
+                    for (Estudiante estudiante : estudiantes2023.getEstudiantes()) {
+                        System.out.println(estudiante.getCodigo() + " " + estudiante.getNombre() + " " + estudiante.getCiclo() + " " + estudiante.getPension());
+                    }
+                    break;
+
+                case 7:
+                    // ESTUDIANTES ORDENADOS POR APELLIDOS
+                    estudiantes2023.listarEstudiantesPorApellido();
+                    System.out.println("****** ESTUDIANTES ORDENADOS POR APELLIDOS ********");
+                    for (Estudiante estudiante : estudiantes2023.getEstudiantes()) {
+                        System.out.println(estudiante.getCodigo() + " " + estudiante.getNombre() + " " + estudiante.getCiclo() + " " + estudiante.getPension());
+                    }
+                    break;
+
+                case 8:
+                    // ESTUDIANTES ORDENADOS POR PENSION ASCENDENTE
+                    estudiantes2023.listarEstudiantesPorPensionAscendente();
+                    System.out.println("****** ESTUDIANTES ORDENADOS POR PENSION ASCENDENTE ********");
+                    for (Estudiante estudiante : estudiantes2023.getEstudiantes()) {
+                        System.out.println(estudiante.getCodigo() + " " + estudiante.getNombre() + " " + estudiante.getCiclo() + " " + estudiante.getPension());
+                    }
+                    break;
+
+                case 9:
+                    // SUMATORIA DE PENSIONES
+                    double sumaPensiones = estudiantes2023.sumaTotalPensiones();
+                    System.out.println("****** SUMATORIA DE PENSIONES ********");
+                    System.out.println("Total de Pensiones: S/ " + sumaPensiones);
+                    break;
+
                 case 10:
                     System.out.println("Saliendo del programa.");
-                    System.exit(0);
                     break;
+
                 default:
                     System.out.println("Opción no válida. Intente nuevamente.");
+                    break;
             }
-        }
+        } while (opcion != 10);
+    }
+}
     }
 
